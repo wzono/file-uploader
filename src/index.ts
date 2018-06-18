006b881da3d9909ee515c39a5938e2f33ec1e0d0
wingsico <wingsico@outlook.com>
2018-06-18 16:55:43 +0800

    重构样式

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import './index.scss'
-import { attachDrag } from './services/drag_'
-import { attachPreview } from './services/preview_'
-import { attachUploadButtons } from './services/button_'
-import { getters, actions} from './store/index'
+import './scss/index.scss'
 
-window.onload = () => {
-  attachDrag()
-  attachPreview()
-  attachUploadButtons()
-  console.log(getters.text())
-  actions.text(["123"])
-  console.log(getters.text())
-}
+// import { attachDrag } from './services/drag_'
+// import { attachPreview } from './services/preview_'
+// import { attachUploadButtons } from './services/button_'
+// import { getters, actions} from './store/index'
+
+// window.onload = () => {
+//   attachDrag()
+//   attachPreview()
+//   attachUploadButtons()
+//   console.log(getters.text())
+//   actions.text(["123"])
+//   console.log(getters.text())
+// }
+
 
 
 
